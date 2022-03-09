81a2e4b68af6a445799dc7eaef44b8a24493f4a9
Olamide Anifowoshe <74327981+LamideAnifowoshe@users.noreply.github.com>
2022-03-10 00:59:29 +0100

    Update Timer.js

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Timer = () => {
   return (
     <div className="flex-container">
-    <div>1</div>
-    <div>2</div>
-    <div>3</div> 
-    <div>4</div>
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
     </div>
-  )
-}
+  );
+};
 
-export default Timer
+export default Timer;
