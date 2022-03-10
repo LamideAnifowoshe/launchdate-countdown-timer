f55d7a8d59033a2899d5a29c7eaa5fca4edc07ac
Olamide Anifowoshe <74327981+LamideAnifowoshe@users.noreply.github.com>
2022-03-10 04:17:22 +0100

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div>
       <h2>We're launching soon</h2>
-      <Timer />
+      <Timer countdownTimestampMs={1643673600000} />
       <Footer />
     </div>
   );
