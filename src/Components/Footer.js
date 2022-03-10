d26c6f50f1483a6693377fa9b9edfb8621e0a417
Olamide Anifowoshe <74327981+LamideAnifowoshe@users.noreply.github.com>
2022-03-10 03:13:03 +0100

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,11 +1,23 @@
-
+import facebook from "./images/facebook.svg";
+import pinterest from "./images/pinterest.svg";
+import instagram from "./images/instagram.svg";
 
 const Footer = () => {
   return (
-    <div>
-    
-    </div>
-  )
-}
+    <div className="footer">
+      <a href="/">
+        <img src={facebook} alt="" />
+      </a>
 
-export default Footer
+      <a href="/">
+        <img src={pinterest} alt="" />
+      </a>
+
+      <a href="/">
+        <img src={instagram} alt="" />
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
