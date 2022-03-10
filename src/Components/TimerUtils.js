1ceb75efade531fba542e560da6b56d4eaa08a67
Olamide Anifowoshe <74327981+LamideAnifowoshe@users.noreply.github.com>
2022-03-10 04:15:53 +0100

    Creae TimerUtils.js

---
+++ src/Components/TimerUtils.js
@@ -0,0 +1,10 @@
+import dayjs from "dayjs";
+
+export function getRemainingTimeUntilMsTimestamo(timestampMs) {
+  return {
+    days: "14",
+    hours: "00",
+    minutes: "00",
+    seconds: "00",
+  };
+}
