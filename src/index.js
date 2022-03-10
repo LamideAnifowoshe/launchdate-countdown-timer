44f360effa4be85a2139f8e530e11d52c032693b
Olamide Anifowoshe <74327981+LamideAnifowoshe@users.noreply.github.com>
2022-03-10 03:10:29 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import stars from "./Components/images/stars.svg";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <App style={{ backgroundImage: { stars } }} />,
+  document.getElementById("root")
+);
 
 reportWebVitals();
