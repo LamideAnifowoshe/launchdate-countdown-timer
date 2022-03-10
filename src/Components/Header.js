1fe2325bde2aa17c0957676806fd135967f8e85c
Olamide Anifowoshe <74327981+LamideAnifowoshe@users.noreply.github.com>
2022-03-10 03:12:49 +0100

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,12 @@
 import Timer from "./Timer";
+import Footer from "./Footer";
 
 const Header = () => {
   return (
     <div>
       <h2>We're launching soon</h2>
       <Timer />
+      <Footer />
     </div>
   );
 };
